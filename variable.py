3fed9acdc09cfa853932e2c5a299e9f76d48b22b
Rajani Empire <rajani.swe.sust@gmail.com>
2026-05-07 23:03:12 +0600

    variable in phython

---
+++ variable.py
@@ -0,0 +1,14 @@
+name = "Rajani"
+rent = 1220
+gas = 202.5
+groceries = 305.6
+
+# print(rent)
+total = rent+gas+groceries
+print("Total amount : ",total)
+
+rent = 1400
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+print("Expense item : ",item1,item2,item3)

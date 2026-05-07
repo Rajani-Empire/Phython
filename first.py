bb3a69724438b133ca70f314c399f029a491b883
Rajani Empire <rajani.swe.sust@gmail.com>
2026-05-07 22:44:28 +0600

    first commit

---
+++ first.py
@@ -0,0 +1,67 @@
+print("Hello")
+print(35+23)
+name = "Rajani"
+age = 23
+price = 25.99
+
+print(name,age,price)
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+
+# sum two number
+num1 = 2
+num2 = 5
+sum = num1+num2
+print(sum)
+
+
+a=50
+b=20
+
+print(a==b)
+print(a!=b)
+
+
+val1 = True
+val2 = False
+
+print ("And operator :",val1 and val2)
+print ("Or operator :",val1 or val2)
+
+# Type conversion and Type casting
+a = int("2")
+b = 4.25
+
+sum = a+b
+
+print(sum)
+print()
+
+name = input("Enter your name : ")
+print("Welcome ",name)
+
+val = int(input("Enter number : "))
+print(type(val),val)
+print()
+
+
+#practice quesdtion1
+num1 = float(input("Enter number1 : "))
+num2 = float(input("Enter number2 : "))
+
+sum1 = num1+num2
+print("Sum = ",sum1)
+
+#practice quesdtion2
+side = int(input("Enter a side : "))
+area = side*side
+
+print ("Area of a square = ",area)
+
+x = int(input("firsr number : "))
+y = int(input("second number : "))
+
+print(x>=y)
